67387fee12bb9a2b73069c914ee76db4128357ad
HTroy97 <hugotroyani@gmail.com>
2021-01-25 17:35:56 +0000

    finished IBM project

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
+function compute()//function to compute result and display results
 {
-    p = document.getElementById("principal").value;
-    
+    principal = document.getElementById("principal").value;
+    if(validate_number(parseInt(principal))){
+        rate=document.getElementById("rate").value;
+        var years=parseInt(document.getElementById("years").value);
+        interest=principal*years*rate/100;
+        var current_date=new Date();
+        var future_year=current_date.getFullYear()+years;
+        document.getElementById('result').innerHTML="If you deposit "+ "<strong>"
+                                                +principal+ "</strong>"+"<br>"+
+                                                " at an interest rate of "+"<strong>"+
+                                                rate+"%"+"</strong>"+"<br>"+
+                                                " you will receive an amount of "+"<strong>"+
+                                                interest+"</strong>"+"<br>"+
+                                                " in the year "+"<strong>"+future_year+"</strong>";
+    }
 }
-        
+
+function range_value(){//function to get the value from the range input and display when it changes
+    var val = document.getElementById('rate').value;
+    document.getElementById('range_output').innerHTML=val+"%";
+}
+
+function validate_number(number){//function we call after submitting to validate if the amount input is a positive number
+    var principal = document.getElementById("principal");
+    if (number>0){
+        return true;      
+    }
+    else{
+        window.alert("Enter a positive number");
+        principal.focus();
+        return false;
+
+    }
+}
